d290f6ec8d2d499f389ddf43d466ae513b3df9d2
arvend <arvendrajan100@gmail.com>
2024-03-03 21:18:34 +0800

    Initial Commit

---
+++ src/main/java/com/medicare/admin/MedicareAppAdminServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medicare.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicareAppAdminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicareAppAdminServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medicare/admin/MedicareAppAdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicare.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicareAppAdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
